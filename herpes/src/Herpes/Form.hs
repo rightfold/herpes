f7c37a2e89f880a915304dfad1db98cfcc7c9557
Foldr <rightfold+kloe@gmail.com>
2018-11-25 18:03:07 +0100

    Add labeled field combinator

--- herpes/src/Herpes/Form.hs
+++ herpes/src/Herpes/Form.hs
@@ -1,13 +1,20 @@
 module Herpes.Form
-  ( Form
+  ( -- * Simple forms
+    Form
   , Field (..)
   , labelField
   , textField
+
+    -- * Composite forms
+  , labeledField
   ) where
 
 import Control.Applicative.Free (Ap, liftAp)
 import Data.Functor.Coyoneda (Coyoneda, liftCoyoneda)
 import Data.Text (Text)
+
+--------------------------------------------------------------------------------
+-- Simple forms
 
 type Form = Ap (Coyoneda Field)
 
@@ -20,3 +27,9 @@ labelField = liftAp . liftCoyoneda . LabelField
 
 textField :: Text -> Form Text
 textField = liftAp . liftCoyoneda . TextField
+
+--------------------------------------------------------------------------------
+-- Composite forms
+
+labeledField :: Text -> Form a -> Form a
+labeledField l f = labelField l *> f
