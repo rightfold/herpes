2bdb72eb7fcd6fff8a6430c8ef2f1d3070fa23f8
Foldr <rightfold+kloe@gmail.com>
2018-11-25 17:44:39 +0100

    Add simple form field constructors

--- herpes/src/Herpes/Form.hs
+++ herpes/src/Herpes/Form.hs
@@ -1,5 +1,14 @@
 module Herpes.Form
-  (
+  ( Form
+  , Field (..)
   ) where
 
+import Control.Applicative.Free (Ap)
+import Data.Functor.Coyoneda (Coyoneda)
+import Data.Text (Text)
 
+type Form = Ap (Coyoneda Field)
+
+data Field :: * -> * where
+  LabelField :: Text -> Field ()
+  TextField :: Field Text
